32c93de492f6e0bfdb0ece8aa7cdbca304728029
phamquocthinh <beelacoste@gmail.com>
2017-02-23 16:38:44 +0700

    Update

--- export.js
+++ export.js
@@ -52,69 +52,24 @@ const cleanText = (string) => {
     return string;
 }
 
-const makePDF = (PDFDocument, docs) => {
+const makePDF = (PDFDocument, doc) => {
     let pdfDoc = new PDFDocument({
         size: [2480, 3508]
     });
 
-    let fileName = 'pdf/';
+    let fileName = 'pdf/' + doc._id + '.pdf';
+    let string = cleanText(doc._id + '_' + doc.name + '_' + doc.address);
 
-    docs.forEach((doc, index) => {
-        switch (index) {
-            case 0:
-                let string1 = cleanText(doc._id + '_' + doc.name + '_' + doc.address);
+    try {
+        pdfDoc.image(imageDirectory + doc.url, 200, 200, { width: 945, height: 945 });
+    } catch (err) {
+        console.log('Can not found image directory for doc: %s', doc._id);
+    }
 
-                fileName = fileName + doc._id;
-
-                try {
-                    pdfDoc.image(imageDirectory + doc.url, 200, 200, { width: 945, height: 945 });
-                    pdfDoc.image('images/1.png', 200, 200, { width: 945, height: 945 });
-                    pdfDoc.image('images/2.png', 222, 222, { width: 900, height: 900 });
-                    pdfDoc.image('images/3.png', 1245, 200, { width: 945, height: 945 });
-                    pdfDoc.font('fonts/times.ttf').fontSize(80).text(string1, 1330, 400, { width: 800, height: 800, align: 'center' });
-                } catch (err) {
-                    console.log(err);
-                }
-
-                break;
-            case 1:
-                let string2 = cleanText(doc._id + '_' + doc.name + '_' + doc.address);
-
-                fileName = fileName + '_' + doc._id;
-
-                try {
-                    pdfDoc.image(imageDirectory + doc.url, 200, 1245, { width: 945, height: 945 });
-                    pdfDoc.image('images/1.png', 200, 1245, { width: 945, height: 945 });
-                    pdfDoc.image('images/2.png', 222, 1267, { width: 900, height: 900 });
-                    pdfDoc.image('images/3.png', 1245, 1245, { width: 945, height: 945 });
-                    pdfDoc.font('fonts/times.ttf').fontSize(80).text(string2, 1330, 1445, { width: 800, height: 800, align: 'center' });
-                } catch (err) {
-                    console.log(err);
-                }
-
-                break;
-            case 2:
-                let string3 = cleanText(doc._id + '_' + doc.name + '_' + doc.address);
-
-                fileName = fileName + '_' + doc._id;
-
-                try {
-                    pdfDoc.image(imageDirectory + doc.url, 200, 2290, { width: 945, height: 945 });
-                    pdfDoc.image('images/1.png', 200, 2290, { width: 945, height: 945 });
-                    pdfDoc.image('images/2.png', 222, 2312, { width: 900, height: 900 });
-                    pdfDoc.image('images/3.png', 1245, 2290, { width: 945, height: 945 });
-                    pdfDoc.font('fonts/times.ttf').fontSize(80).text(string3, 1330, 2490, { width: 800, height: 800, align: 'center' });
-                } catch (err) {
-                    console.log(err);
-                }
-
-                break;
-            default:
-                break;
-        }
-    })
-
-    fileName += '.pdf';
+    pdfDoc.image('images/1.png', 200, 200, { width: 945, height: 945 });
+    pdfDoc.image('images/2.png', 222, 222, { width: 900, height: 900 });
+    pdfDoc.image('images/3.png', 1245, 200, { width: 945, height: 945 });
+    pdfDoc.font('fonts/times.ttf').fontSize(80).text(string, 1330, 400, { width: 800, height: 800, align: 'center' });
 
     pdfDoc.pipe(fs.createWriteStream(fileName));
 
@@ -122,21 +77,14 @@ const makePDF = (PDFDocument, docs) => {
 }
 
 const print = (docs) => {
-    return new Promise((resolve, reject) => {
-        let docsToPrint = docs.splice(0, 3);
+    return Promise.each(docs, (doc, index) => {
+        makePDF(PDFDocument, doc);
 
-        console.log('%s docs left...', docs.length);
-        makePDF(PDFDocument, docsToPrint);
+        console.log('%s docs left...', docs.length - index -1);
 
         setTimeout(() => {
             resolve();
         }, 100)
-    }).then(() => {
-        if (docs.length <= 0) {
-            return Promise.resolve()
-        }
-
-        return print(docs);
     })
 }
 
@@ -153,13 +101,13 @@ const execute = () => {
 
             return print(docs);
         })
-        .then(() => {
-            console.log('Finished');
-
-            setTimeout(() => {
-                process.exit();
-            }, 10000)
-        })
 }
 
-execute()
+execute().then(() => {
+    console.log('Finished');
+    console.log('Exit after 5 minutes...')
+
+    setTimeout(() => {
+        process.exit();
+    }, 5 * 60 * 1000)
+})
